a182d6efeb20af4929ad5c588e0a35e2a0bb1253
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-03 22:17:22 +0530

    set client Socket

--- INP-Study-project/Module_01/src/Client.java
+++ INP-Study-project/Module_01/src/Client.java
@@ -6,5 +6,8 @@ public class Client {
     public static void main(String[] args) throws IOException {
         final int PORT=8003;
 
+        Socket Socket = new Socket("localhost",PORT);
+
+
     }
 }
