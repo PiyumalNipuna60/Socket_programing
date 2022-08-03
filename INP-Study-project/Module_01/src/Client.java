a7090ac4f34021568d51d59be3ae13d7f933c571
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-03 22:15:35 +0530

    set ServerSocket & pass Local Socket

--- INP-Study-project/Module_01/src/Client.java
+++ INP-Study-project/Module_01/src/Client.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final int PORT=8003;
 
     }
--- INP-Study-project/Module_01/src/Server.java
+++ INP-Study-project/Module_01/src/Server.java
@@ -1,6 +1,13 @@
-public class Server {
-    public static void main(String[] args) {
-        final int PORT=8003;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        final int PORT=8003;
+        ServerSocket serverSocket=new ServerSocket(PORT);
+        Socket localSocket = serverSocket.accept();
+        System.out.println("Port : "+localSocket.getPort());
+        System.out.println("IP : "+localSocket.getInetAddress());
     }
 }
