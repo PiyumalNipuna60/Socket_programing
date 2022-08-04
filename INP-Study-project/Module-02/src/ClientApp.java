2769d918d95ee604da3801160077126bad2e4c4e
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-04 22:48:50 +0530

    Module-02: set client Socket

--- INP-Study-project/Module-02/src/ClientApp.java
+++ INP-Study-project/Module-02/src/ClientApp.java
@@ -1,6 +1,9 @@
-public class ClientApp {
-    public static void main(String[] args) {
-        final int PORT=8006;
+import java.io.IOException;
+import java.net.Socket;
 
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        final int PORT=8006;
+        Socket localhost = new Socket("localhost", PORT);
     }
 }
