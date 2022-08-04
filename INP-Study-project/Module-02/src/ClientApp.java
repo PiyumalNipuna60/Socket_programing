18ff165972c61b28dd7152adf532be32b5b6c0e4
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-04 23:39:55 +0530

    Module-02: set Client  massage print and reply

--- INP-Study-project/Module-02/src/ClientApp.java
+++ INP-Study-project/Module-02/src/ClientApp.java
@@ -1,10 +1,9 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientApp {
-    public static void main(String[] args){
-        final int PORT=8006;
+    public static void main(String[] args) {
+        final int PORT = 8006;
 
         try {
             Socket localhost = new Socket("localhost", PORT);
@@ -17,7 +16,17 @@ public class ClientApp {
             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
+            String massage = "", reply = "";
+            while (massage.equals("Exit")) {
+                //input Massage
+                massage = dataInputStream.readUTF();
+                System.out.println(massage);
 
+                //reply massage
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
