3d2fa9684a561aed7595628202f4bf6a904c5e29
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-04 22:46:48 +0530

    Module-02: set ServerSocket & pass Local Socket

--- INP-Study-project/Module-02/src/ServerApp.java
+++ INP-Study-project/Module-02/src/ServerApp.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final int PORT=8006;
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        Socket localSocket = serverSocket.accept();
+        System.out.println("Port : "+localSocket.getPort());
+        System.out.println("IP : "+localSocket.getInetAddress());
     }
 }
+
