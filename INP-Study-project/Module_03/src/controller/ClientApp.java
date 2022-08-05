3871496480095d2ab5c7b5d43b26151e329f09d6
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-05 23:23:23 +0530

    Module-03 : Set Port

--- INP-Study-project/Module_03/src/controller/ClientApp.java
+++ INP-Study-project/Module_03/src/controller/ClientApp.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ClientApp {
+    public TextArea txtAreaMsg;
+    public TextField txtMsg;
+    public Button btnSent;
+
+    final int Port=8007;
+
+
 }
--- INP-Study-project/Module_03/src/controller/ServerApp.java
+++ INP-Study-project/Module_03/src/controller/ServerApp.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ServerApp {
+    public Button btnSent;
+    public TextArea txtAreaMsg;
+    public TextField txtMsg;
+
+
 }
