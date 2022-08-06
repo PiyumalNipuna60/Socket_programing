83a08c28131079b70b53167eec87fcfc3b3d9819
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-06 17:54:41 +0530

    Module-03 : Massage And Reply print txtArea for Client

--- INP-Study-project/Module_03/src/controller/ClientApp.java
+++ INP-Study-project/Module_03/src/controller/ClientApp.java
@@ -5,9 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientApp {
@@ -19,6 +17,8 @@ public class ClientApp {
     DataOutputStream dataOutputStream;
     DataInputStream dataInputStream;
 
+    String massage="", reply="";
+
     public void initialize() {
         new Thread(()->{
             try {
@@ -27,6 +27,19 @@ public class ClientApp {
 
                 dataOutputStream=new DataOutputStream(socket.getOutputStream());
                 dataInputStream=new java.io.DataInputStream(socket.getInputStream());
+
+                InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+                while (massage.equals("Exit")){
+                    massage=dataInputStream.readUTF();
+                    txtAreaMsg.appendText(massage);
+
+                    reply=bufferedReader.readLine();
+                    dataOutputStream.writeUTF(reply);
+                    dataOutputStream.flush();
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
